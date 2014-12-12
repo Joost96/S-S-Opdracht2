349dc655d355e46ef0c791d13dc0e20b936f85e0
Alexander van den Herik <herik001@hva.nl>
2014-12-12 09:52:19 +0100

    stopwatch

--- S&S-Opdracht2/src/s/s/opdracht2/Main.java
+++ S&S-Opdracht2/src/s/s/opdracht2/Main.java
@@ -1,6 +1,8 @@
 package s.s.opdracht2;
 
 import PO2Library.EdgeWeightedDigraph;
+import nl.hva.dmci.ict.inf.ads.lib.Stopwatch;
+
 
 /**
  *
@@ -12,12 +14,18 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        
         EdgeWeightedDigraph EWD = new EdgeWeightedDigraph("i1");
+        Stopwatch stpw = new Stopwatch();
         Dijkstra dijkstra = new Dijkstra(EWD, EWD.getStart());
         System.out.println(dijkstra.distTo(EWD.getEnd()));
         EWD.tekenPad(dijkstra.pathTo(EWD.getEnd()));
+        System.out.println(stpw.elapsedTime());
         EWD.save("i1-1");
         System.out.println(dijkstra.count);
+        
+        
         
     }
     
